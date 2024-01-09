c1fe68b6a1144d575524f8e8c9567c900698a7b3
All1in <bm3023068@gmail.com>
2024-01-09 15:49:00 +0200

    socket handlers done

--- index.js
+++ index.js
@@ -17,4 +17,21 @@ app.get('/', (req, res) => {
     res.send('Server is running');
 });
 
+// my socket handlers
+io.on("connection", (socket) => {
+    socket.emit("me", socket.id);
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded")
+    });
+
+    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+    });
+
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal)
+    });
+});
+
 server.listen(PORT, () => console.log(`Server is on PORT: ${PORT}`))
